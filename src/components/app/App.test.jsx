8bab638996e8667d65788595cfa589dfe47b9378
Gabriel Simek <gabrielsimek@outlook.com>
2021-08-16 17:27:12 -0700

    initial tests passing

---
+++ src/components/app/App.test.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from './App';
+
+describe('App component', () => {
+  it('renders App', () => {
+    render(<App/>);
+    const display = screen.getByTestId('display');
+    const colorInput = screen.getByTestId('color-input');
+    const undoButton = screen.getByText('undo');
+    const redoButton = screen.getByText('redo');
+
+    expect(display).toHaveStyle({ backgroundColor: '#FF0000' });
+
+    fireEvent.change(colorInput, { target: { value: '#0000FF' } });
+    expect(display).toHaveStyle({ backgroundColor: '#0000FF' });
+
+    fireEvent.click(undoButton);
+    expect(display).toHaveStyle({ backgroundColor: '#FF0000' });
+
+    fireEvent.click(redoButton);
+    expect(display).toHaveStyle({ backgroundColor: '#0000FF' });
+
+
+
+  });
+});
